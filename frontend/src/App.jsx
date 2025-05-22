e71fc171f709138de5ebabb62aa7aeeeff728932
Akshatverma01 <akshat0104verma@gmail.com>
2025-05-22 23:34:21 +0530

    fix supabase error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
+import { Button } from '@mui/material'
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+   <div className="p-4 bg-gray-100">
+    <h1 className="text-xl font-bold mb-4">Hello with Tailwind</h1>
+    <Button variant="contained" color="primary">
+      MUI Button
+    </Button>
+  </div>
   )
 }
 
