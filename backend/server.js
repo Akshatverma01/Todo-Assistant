9b9f44b7cfe72033cb3492a289d08ece2fb857c8
Akshatverma01 <akshat0104verma@gmail.com>
2025-05-23 18:19:56 +0530

    deploy setup

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://todo-assistant-rho.vercel.app/"||"http://localhost:5173",
     credentials: true,
   })
 );
