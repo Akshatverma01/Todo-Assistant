1d15520e9766f5607e82878caa85bc655a336f3b
Akshatverma01 <akshat0104verma@gmail.com>
2025-05-24 00:06:42 +0530

    update deploy

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(cors({
-    origin: "https://todo-assistant-rho.vercel.app",
-    credentials: true,
-  }));
+const options = {
+  origin: [
+    "https://todo-assistant-rho.vercel.app",
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", ],
+}
+app.use(cors(options));
 
 app.use("/todo", todoRoutes);
 app.use("/summarize", summarizeRoute)
